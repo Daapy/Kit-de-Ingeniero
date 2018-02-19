c698f46494897241e1ba217dc671398710ed1acd
RaulCortes885 <36635671+RaulCortes885@users.noreply.github.com>
2018-02-19 10:33:03 -0600

    Masa
    Ya cambie la perra masa y ya funciona

--- KitIngeniero/KitIngeniero/Conversion de Unidades.cs
+++ KitIngeniero/KitIngeniero/Conversion de Unidades.cs
@@ -16,5 +16,68 @@ namespace KitIngeniero
         {
             InitializeComponent();
         }
+
+        private void btnConvA_Click(object sender, EventArgs e)
+        {
+            if (btnKiloA.selected==true)
+            {
+                if (btnOnzaB.selected==true)
+                {
+                    txbResA.Text= (Convert.ToInt16(txbUnidadA.Text) * (35.274)).ToString();
+
+                }
+                if (btnLibB.selected==true)
+                {
+                    txbResA.Text = (Convert.ToInt16(txbUnidadA.Text) * (2.20462)).ToString();
+                }
+            }
+
+            if (btnOnzaA.selected == true)
+            {
+                if (btnKiloB.selected == true)
+                {
+                    txbResA.Text = (Convert.ToInt16(txbUnidadA.Text) * (0.0283495)).ToString();
+
+                }
+                if (btnLibB.selected == true)
+                {
+                    txbResA.Text = (Convert.ToInt16(txbUnidadA.Text) * (0.0625)).ToString();
+                }
+            }
+
+            if (btnLibA.selected == true)
+            {
+                if (btnKiloB.selected == true)
+                {
+                    txbResA.Text = (Convert.ToInt16(txbUnidadA.Text) * (0.453592)).ToString();
+
+                }
+                if (btnOnzaB.selected == true)
+                {
+                    txbResA.Text = (Convert.ToInt16(txbUnidadA.Text) * (16)).ToString();
+                }
+            }
+        }
+
+        private void btnKiloA_Click(object sender, EventArgs e)
+        {
+            btnKiloB.Enabled = false;
+            btnOnzaB.Enabled = true;
+            btnLibB.Enabled = true;
+        }
+
+        private void btnOnzaA_Click(object sender, EventArgs e)
+        {
+            btnKiloB.Enabled = true;
+            btnOnzaB.Enabled = false;
+            btnLibB.Enabled = true;
+        }
+
+        private void btnLibA_Click(object sender, EventArgs e)
+        {
+            btnKiloB.Enabled = true;
+            btnOnzaB.Enabled = true;
+            btnLibB.Enabled = false;
+        }
     }
 }
