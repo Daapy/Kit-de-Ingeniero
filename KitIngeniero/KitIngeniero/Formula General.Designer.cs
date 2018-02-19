01e711c56c7abf511b68dfee57b6a1fee51a40f8
MIKE <MiguelMartinezDW@outlook.com>
2018-02-19 09:39:11 -0600

    mi programación
    PERRA

--- KitIngeniero/KitIngeniero/Formula General.Designer.cs
+++ KitIngeniero/KitIngeniero/Formula General.Designer.cs
@@ -50,18 +50,18 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Nunito", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.White;
             this.label2.Location = new System.Drawing.Point(120, 33);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(128, 21);
+            this.label2.Size = new System.Drawing.Size(128, 20);
             this.label2.TabIndex = 22;
             this.label2.Text = "Formula General";
             // 
             // bunifuMaterialTextbox1
             // 
             this.bunifuMaterialTextbox1.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.bunifuMaterialTextbox1.Font = new System.Drawing.Font("Nunito", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bunifuMaterialTextbox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.bunifuMaterialTextbox1.ForeColor = System.Drawing.Color.WhiteSmoke;
             this.bunifuMaterialTextbox1.HintForeColor = System.Drawing.Color.Empty;
             this.bunifuMaterialTextbox1.HintText = "";
@@ -80,7 +80,7 @@
             // bunifuMaterialTextbox2
             // 
             this.bunifuMaterialTextbox2.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.bunifuMaterialTextbox2.Font = new System.Drawing.Font("Nunito", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bunifuMaterialTextbox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.bunifuMaterialTextbox2.ForeColor = System.Drawing.Color.WhiteSmoke;
             this.bunifuMaterialTextbox2.HintForeColor = System.Drawing.Color.Empty;
             this.bunifuMaterialTextbox2.HintText = "";
@@ -99,7 +99,7 @@
             // bunifuMaterialTextbox3
             // 
             this.bunifuMaterialTextbox3.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.bunifuMaterialTextbox3.Font = new System.Drawing.Font("Nunito", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bunifuMaterialTextbox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.bunifuMaterialTextbox3.ForeColor = System.Drawing.Color.WhiteSmoke;
             this.bunifuMaterialTextbox3.HintForeColor = System.Drawing.Color.Empty;
             this.bunifuMaterialTextbox3.HintText = "";
@@ -124,7 +124,7 @@
             this.btnLimpiar.ButtonText = "Limpiar";
             this.btnLimpiar.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnLimpiar.DisabledColor = System.Drawing.Color.Gray;
-            this.btnLimpiar.Font = new System.Drawing.Font("Nunito SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpiar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnLimpiar.Iconcolor = System.Drawing.Color.Transparent;
             this.btnLimpiar.Iconimage = null;
             this.btnLimpiar.Iconimage_right = null;
@@ -149,7 +149,8 @@
             this.btnLimpiar.Text = "Limpiar";
             this.btnLimpiar.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.btnLimpiar.Textcolor = System.Drawing.Color.White;
-            this.btnLimpiar.TextFont = new System.Drawing.Font("Nunito", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpiar.TextFont = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
             // 
             // btnResvrMatriz
             // 
@@ -160,7 +161,7 @@
             this.btnResvrMatriz.ButtonText = "Resolver";
             this.btnResvrMatriz.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnResvrMatriz.DisabledColor = System.Drawing.Color.Gray;
-            this.btnResvrMatriz.Font = new System.Drawing.Font("Nunito SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnResvrMatriz.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnResvrMatriz.Iconcolor = System.Drawing.Color.Transparent;
             this.btnResvrMatriz.Iconimage = null;
             this.btnResvrMatriz.Iconimage_right = null;
@@ -185,82 +186,83 @@
             this.btnResvrMatriz.Text = "Resolver";
             this.btnResvrMatriz.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.btnResvrMatriz.Textcolor = System.Drawing.Color.FromArgb(((int)(((byte)(47)))), ((int)(((byte)(52)))), ((int)(((byte)(60)))));
-            this.btnResvrMatriz.TextFont = new System.Drawing.Font("Nunito", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnResvrMatriz.TextFont = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnResvrMatriz.Click += new System.EventHandler(this.btnResvrMatriz_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Nunito", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
             this.label1.Location = new System.Drawing.Point(52, 96);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(17, 18);
+            this.label1.Size = new System.Drawing.Size(17, 16);
             this.label1.TabIndex = 38;
             this.label1.Text = "A";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Nunito", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.White;
             this.label3.Location = new System.Drawing.Point(169, 96);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(17, 18);
+            this.label3.Size = new System.Drawing.Size(17, 16);
             this.label3.TabIndex = 39;
             this.label3.Text = "B";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Nunito", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.White;
             this.label4.Location = new System.Drawing.Point(290, 96);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(17, 18);
+            this.label4.Size = new System.Drawing.Size(17, 16);
             this.label4.TabIndex = 40;
             this.label4.Text = "C";
             // 
             // lblX2
             // 
             this.lblX2.AutoSize = true;
-            this.lblX2.Font = new System.Drawing.Font("Nunito", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblX2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblX2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(182)))), ((int)(((byte)(51)))));
             this.lblX2.Location = new System.Drawing.Point(236, 9);
             this.lblX2.Name = "lblX2";
-            this.lblX2.Size = new System.Drawing.Size(16, 18);
+            this.lblX2.Size = new System.Drawing.Size(15, 16);
             this.lblX2.TabIndex = 44;
             this.lblX2.Text = "5";
             // 
             // lblX1
             // 
             this.lblX1.AutoSize = true;
-            this.lblX1.Font = new System.Drawing.Font("Nunito", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblX1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblX1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(182)))), ((int)(((byte)(51)))));
             this.lblX1.Location = new System.Drawing.Point(101, 9);
             this.lblX1.Name = "lblX1";
-            this.lblX1.Size = new System.Drawing.Size(22, 18);
+            this.lblX1.Size = new System.Drawing.Size(19, 16);
             this.lblX1.TabIndex = 43;
             this.lblX1.Text = "-1";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Nunito", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.White;
             this.label5.Location = new System.Drawing.Point(195, 9);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(35, 18);
+            this.label5.Size = new System.Drawing.Size(33, 16);
             this.label5.TabIndex = 42;
             this.label5.Text = "X2 =";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Nunito", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.ForeColor = System.Drawing.Color.White;
             this.label6.Location = new System.Drawing.Point(64, 9);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(35, 18);
+            this.label6.Size = new System.Drawing.Size(33, 16);
             this.label6.TabIndex = 41;
             this.label6.Text = "X1 =";
             // 
@@ -268,11 +270,11 @@
             // 
             this.label8.AutoSize = true;
             this.label8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(62)))), ((int)(((byte)(70)))));
-            this.label8.Font = new System.Drawing.Font("Nunito", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label8.ForeColor = System.Drawing.Color.White;
             this.label8.Location = new System.Drawing.Point(8, 286);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(69, 18);
+            this.label8.Size = new System.Drawing.Size(73, 16);
             this.label8.TabIndex = 60;
             this.label8.Text = "Resultado:";
             // 
@@ -292,13 +294,12 @@
             // 
             this.label9.AutoSize = true;
             this.label9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(62)))), ((int)(((byte)(70)))));
-            this.label9.Font = new System.Drawing.Font("Nunito", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label9.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(69)))), ((int)(((byte)(52)))));
             this.label9.Location = new System.Drawing.Point(150, 354);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(53, 18);
+            this.label9.Size = new System.Drawing.Size(0, 16);
             this.label9.TabIndex = 61;
-            this.label9.Text = "ERROR";
             // 
             // Formula_General
             // 
--- KitIngeniero/KitIngeniero/Formula General.cs
+++ KitIngeniero/KitIngeniero/Formula General.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using formulaGeneral;
 
 namespace KitIngeniero
 {
@@ -15,6 +16,54 @@ namespace KitIngeniero
         public Formula_General()
         {
             InitializeComponent();
+        }
+
+        #region Funciones para el procesamiento y muestra de resultados
+        /*
+         * Funcion para la llamada del procesamiento y la precentación de resultados
+         */
+        void calculo()
+        {
+            try
+            {
+                // Variables locales
+                double a = Convert.ToDouble(bunifuMaterialTextbox1.Text);
+                double b = Convert.ToDouble(bunifuMaterialTextbox2.Text);
+                double c = Convert.ToDouble(bunifuMaterialTextbox3.Text);
+                //Llamada del metodo
+                double[] x = FG.calcularFG(a, b, c);
+                // Muestra los resultados
+                lblX1.Text = x[0].ToString();
+                lblX2.Text = x[1].ToString();
+            }
+            catch (Exception)
+            {
+                label9.Text = "ERROR";
+            }
+        }
+
+        /*
+         * Funcion para limpiar las cajas de texto y los resultados
+         */
+        void limpiar()
+        {
+            lblX1.Text = "";
+            lblX2.Text = "";
+            bunifuMaterialTextbox1.Text = "";
+            bunifuMaterialTextbox2.Text = "";
+            bunifuMaterialTextbox3.Text = "";
+            label9.Text = "";
+        }
+        #endregion
+
+        private void btnResvrMatriz_Click(object sender, EventArgs e)
+        {
+            calculo();
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            limpiar();
         }
     }
 }
